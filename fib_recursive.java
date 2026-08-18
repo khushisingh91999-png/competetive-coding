9b3f46b3079349aafa6b04d0f900ea6b54cb4ff9
khushisingh91999-png <khushisingh91999@gmail.com>
2026-08-18 12:07:58 +0530

    added fib recursive

---
+++ fib_recursive.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fib = Fibonacci(n);
+    }
+    
+    public static int Fibonacci(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        
+        return Fibonacci(n-1) + Fibonacci(n-2);
+    }
+}w

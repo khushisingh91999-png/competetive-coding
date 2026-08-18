6650c135f66806bc18b030b107df66e7ff2d4eeb
khushisingh91999-png <khushisingh91999@gmail.com>
2026-08-18 12:30:21 +0530

    added ascend

---
+++ ascending.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class firstprog
+{
+    public static void main(String args[])
+    {
+        Scanner Sc = new Scanner(System.in);
+        int n = Sc.nextInt();
+        {
+            Ascending(int n);
+            for(int i=1; i<=n; i++)
+            {   
+                System.out.println(i);
+            }
+        }  
+    }
+}

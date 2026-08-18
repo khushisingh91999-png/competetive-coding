c440c92f2470d4fb88e4381e12a1f67e2f41a4b5
khushisingh91999-png <khushisingh91999@gmail.com>
2026-08-18 12:22:30 +0530

    added odd,even

---
+++ odd,even.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class firstprog
+{
+    public static void main(String args[])
+    {
+        int a[] = {2,5,7,9,4,8,3,6};
+        {
+            System.out.println("even numbers:");
+            for(int i=0;i<a.length;i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    System.out.println(a[i]);
+                }
+                else
+                {
+                    
+                }
+            }
+            
+            System.out.println("odd numbers:");
+            for(int i=0;i<a.length;i++)
+            {
+                if (a[i] % 2 ==0)
+                {
+                    
+                }
+                else
+                {
+                    System.out.println(a[i]);
+                }
+            }
+        }
+    }
+}

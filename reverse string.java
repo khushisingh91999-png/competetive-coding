edc40e1b470a9103250332a9ba8d7cfdb557bc59
khushisingh91999-png <khushisingh91999@gmail.com>
2026-08-19 22:27:47 +0530

    added reverse string

---
+++ reverse string.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    //public class ReverseString {
+        String str = "Hello";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversed);
+
+	}
+}
